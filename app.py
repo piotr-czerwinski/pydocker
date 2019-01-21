8df1173cbe77ae76e1ae96b3cecbadd8e2c8b55e
Piotr Czerwiński <piotr.czerwinski.dev@gmail.com>
2019-01-21 20:59:17 +0100

    add alchemy usage

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ from web import app
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
+
+
+
+from web import db
+from web.models import Ticker, TickerSubscription,User
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Ticker': Ticker, 'TickerSubscription': TickerSubscription, 'User': User}
--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from web import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
+from web import routes, models
+
+from web.models import User
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
