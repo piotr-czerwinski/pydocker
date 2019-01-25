aea71a7be83020ecd0637b755152595ce1a8af8b
Piotr Czerwiński <piotr.czerwinski.dev@gmail.com>
2019-01-25 22:33:17 +0100

    test

---
+++ ticker_test.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+import unittest
+from web import app, db
+from web.models import Ticker
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_unique_name(self):
+        t1 = Ticker(name='BTCUSD')
+        db.session.add(t1)
+        db.session.commit()
+
+        t2 = Ticker(name='BTCUSD')
+        db.session.add(t2)
+        
+        db.session.commit()
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- web/models.py
+++ web/models.py
@@ -2,6 +2,11 @@ from web import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+# followers = db.Table('followers',
+#     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+#     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+# )
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -9,6 +14,11 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
 
     subscriptions = db.relationship('TickerSubscription', backref='user', lazy='dynamic')
+    # followed = db.relationship(
+    #     'User', secondary=followers,
+    #     primaryjoin=(followers.c.follower_id == id),
+    #     secondaryjoin=(followers.c.followed_id == id),
+    #     backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -35,3 +45,4 @@ class TickerSubscription(db.Model):
 
     def __repr__(self):
         return '<Ticker {}>'.format(self.ticker_id)
+
