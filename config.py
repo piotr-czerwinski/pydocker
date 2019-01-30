62027803fe4e71f615f9278deb6aa8214b1a61c4
Piotr Czerwiński <piotr.czerwinski.dev@gmail.com>
2019-01-30 16:29:18 +0100

    .env file support

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
@@ -7,6 +10,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    print(SQLALCHEMY_DATABASE_URI)
+    print(SECRET_KEY)
     SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
     SENDGRID_DEFAULT_FROM = os.getenv('SENDGRID_DEFAULT_FROM') or 'piotr.czerw@gmail.com'
